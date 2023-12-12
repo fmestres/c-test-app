98753916da354645902398a73d02ab48410e6352
fmestres <59103282+fmestres@users.noreply.github.com>
2023-12-12 14:36:43 -0300

    Update processing.py

--- src/processing.py
+++ src/processing.py
@@ -63,47 +63,5 @@ def get_text_with_blanks(text: str, frequency: int, ratio: float = 0.5) -> Tuple
     return new_text, answers
 
 
-text = '''Inß recent years, the integration of artificial intelligence (AI) into healthcare systems has sparked significant transformations, revolutionizing the medical field. AI's unparalleled capacity to analyze vast amounts of data and recognize patterns has led to breakthroughs in disease diagnosis, personalized treatment plans, and drug discovery.
-
-Advanced AI algorithms can identify subtle anomalies in medical images like MRIs and CT scans, enhancing early detection of diseases such as cancer. Additionally, AI-driven predictive modeling assists clinicians in foreseeing potential health risks in patients, enabling proactive interventions. Drug development has also benefited, with AI-driven simulations expediting the identification of potential drug candidates and their interactions.'''
-
-print(get_text_with_blanks(text, 5, 0.3))
 
 
-
-
-''' Kept just in case
-
-def get_sanitized_words(text: str) -> List[str]:
-
-    text = re.sub(r'[^a-zA-Z0-9 ]', '', text)
-    text = text.lower()
-    return text.split()
-
-
-def get_half_word_dict(words: List[str], frequency: int) -> Tuple[Dict[str, str], Dict[int, str]]:
-
-    half_word_dict = {}
-    answers = {}
-    indices = (frequency * i for i in range(len(words) // frequency))
-    j = 0
-    for i in indices:
-        word = words[i]
-        if word not in half_word_dict:
-            word_len = len(word)
-            half_word = word[:word_len // 2] + '_' * (word_len - word_len // 2) + '(' + str(j) +')'
-            half_word_dict[word] = half_word
-            answers[j] = word[word_len // 2:]
-            j += 1
-
-    return half_word_dict, answers
-    
-
-def replace_into_text(text, half_word_dict):
-    
-    for word, half_word in half_word_dict.items():
-        text = re.sub(r'\b' + re.escape(word) + r'\b', half_word, text)
-    return text
-
-
-'''
